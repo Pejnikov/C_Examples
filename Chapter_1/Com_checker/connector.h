28f1da491ded9b2f1fdac468d9c8d229ca338110
IPejnikov <IPejnikov@gmail.com>
2018-10-01 21:54:57 +0300

    Some Remarks

---
+++ Chapter_1/Com_checker/connector.h
@@ -0,0 +1,4 @@
+#ifndef connector
+#define connector
+void con_by_serial();
+#endif
---
+++ Chapter_1/Com_checker/main.c
@@ -0,0 +1,9 @@
+#include<windows.h>
+#include<stdio.h>
+#include "connector.h"
+
+int main()
+{
+    con_by_serial();
+    return 0;
+}
